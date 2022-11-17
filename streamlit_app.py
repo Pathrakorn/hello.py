a3a096271ea7e709424feb993d7c9a8175beeab3
Pathrakorn <111419591+Pathrakorn@users.noreply.github.com>
2022-11-17 16:41:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,16 @@
 import streamlit as st
+import plotly.graph_objects as go 
 
-st.title('🎈 App Name')
 
-st.write('Hello world!')
+
+#-------------settings---------------
+incomes = ["1","2","3"]
+expenses = ["1","2","3"]
+currency = "THB"
+Page_tittle = "Income and Expense tracker"
+page_itcon = ":money_with_wings:"
+layout = "centered"
+#------------------------------------
+
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout,layout)
+st.title(page_title + " " + page_icon)
